d9ddb11dad5fc50d9bc6fb438091fd88af706844
epostnikof <epostnikof@yandex.ru>
2021-12-05 22:33:11 +0300

    add_2_scripts

---
+++ fix_BT.sh
@@ -0,0 +1,2 @@
+cd /home/mrrobot/big_data_HDD_789GB/Soft/20200806_LINUX_BT_DRIVER_RTL8761B_COEX_v0202/20200806_LINUX_BT_DRIVER_RTL8761B_COEX_v0202
+sudo make install INTERFACE=all || sudo make install INTERFACE=usb || sudo make install INTERFACE=uart
